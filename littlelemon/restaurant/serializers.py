16dcecb11c6a59db1088421f35dc0ec9f519509b
kamalaznlv <kamala.znlv@gmail.com>
2024-10-16 17:22:32 +0400

    16/10 changes

---
+++ littlelemon/restaurant/serializers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User 
+from rest_framework import serializers 
+from .models import Menu, Booking
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu 
+        fields = ['id', 'title', 'price', 'inventory']
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+class Meta: 
+    model = User 
+    fields = ['url', 'username', 'email', 'groups']
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+# Handles GET (list) and POST (create) for menu items
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all() 
+    serializer_class = MenuSerializer 
+
+# Handles GET (retrieve), PUT (update), and DELETE for a single menu item
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
